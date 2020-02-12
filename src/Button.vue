d827050da00aaf74c6a6d03a71ffef059f9cbc64
ZinkWu <wugu2599l@gmail.com>
2020-02-12 15:21:37 +0800

    添加 icon 和 iconPosition 属性

--- src/Button.vue
+++ src/Button.vue
@@ -1,9 +1,33 @@
 <template>
-    <button class="v-button">hello</button>
+    <button class="v-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg class="icon" v-if="icon">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data(){
+      return {
+
+      }
+    },
+    // props: ['icon', 'iconPosition']
+    props: {
+      icon: {
+        type: String,
+        default: ""
+      },
+      iconPosition: {
+        type: String,
+        default: ""
+      }
+    }
+  }
 </script>
 
 <style scoped lang="scss">
@@ -14,14 +38,19 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
-        &:hover{
-            border-color: var(--border-hover-color);
-        }
-        &:active{
-            background: var(--button-active-bg);
-        }
-        &:focus{
-            outline: none;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+        &:hover{border-color: var(--border-hover-color);}
+        &:active{background: var(--button-active-bg);}
+        &:focus{outline: none;}
+        >.icon{width: 1em;height: 1.2em;order: 1;margin-right: .1em;margin-left: 0}
+        >.content{order: 2; line-height: 1em}
+        &.icon-right{
+            >.icon{order: 2;margin-right: 0;margin-left: .1em}
+            >.content{order: 1}
         }
     }
 </style>
+
