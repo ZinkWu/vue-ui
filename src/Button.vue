fcd8489667600720486146069105a7cd7bb7ccb7
ZinkWu <wugu2599l@gmail.com>
2020-02-14 14:36:09 +0800

    验证 iconPosition 为 left or right 中的一个 添加 Icon.vue

--- src/Button.vue
+++ src/Button.vue
@@ -1,8 +1,6 @@
 <template>
     <button class="v-button" :class="{[`icon-${iconPosition}`]: true}">
-        <svg class="icon" v-if="icon">
-            <use :xlink:href=`#i-${icon}`></use>
-        </svg>
+        <v-icon v-if="icon" :name="icon" class="icon"></v-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -16,15 +14,14 @@
 
       }
     },
-    // props: ['icon', 'iconPosition']
     props: {
-      icon: {
-        type: String,
-        default: ""
-      },
+      icon: {},
       iconPosition: {
         type: String,
-        default: ""
+        default: "left",
+        validator(val){
+          return val === 'left' || val === 'right'
+        }
       }
     }
   }
@@ -32,21 +29,17 @@
 
 <style scoped lang="scss">
     .v-button{
-        font-size: var(--font-szie);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
-        vertical-align: middle;
+        font-size: var(--font-szie);height: var(--button-height);padding: 0 1em;border-radius: var(--border-radius);border: 1px solid var(--border-color);
+        background: var(--button-bg);display: inline-flex;justify-content: center;align-items: center;vertical-align: middle;
         &:hover{border-color: var(--border-hover-color);}
         &:active{background: var(--button-active-bg);}
         &:focus{outline: none;}
-        >.icon{width: 1em;height: 1.2em;order: 1;margin-right: .1em;margin-left: 0}
-        >.content{order: 2; line-height: 1em}
+        >.icon{width: 1em;height: 1.2em;}
+        >.content{line-height: 1em}
+        &.icon-left{
+            >.icon{order: 1;margin-right: .1em;margin-left: 0}
+            >.content{order: 2}
+        }
         &.icon-right{
             >.icon{order: 2;margin-right: 0;margin-left: .1em}
             >.content{order: 1}
--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Vue from "vue"
 import Button from "./Button";
+import Icon from "./Icon"
 
 Vue.component('v-button', Button)
+Vue.component('v-icon', Icon)
 
 new Vue({
   el: "#app"
