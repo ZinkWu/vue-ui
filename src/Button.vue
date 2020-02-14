76c6f1f19b6eb58f1e0b8906681154092e72926d
ZinkWu <wugu2599l@gmail.com>
2020-02-14 15:12:04 +0800

    添加 loading status

--- src/Button.vue
+++ src/Button.vue
@@ -1,19 +1,15 @@
 <template>
     <button class="v-button" :class="{[`icon-${iconPosition}`]: true}">
         <v-icon v-if="icon" :name="icon" class="icon"></v-icon>
+        <v-icon class="loading" name="loading"></v-icon>
         <div class="content">
-            <slot></slot>
+            <slot/>
         </div>
     </button>
 </template>
 
 <script>
   export default {
-    data(){
-      return {
-
-      }
-    },
     props: {
       icon: {},
       iconPosition: {
@@ -28,6 +24,10 @@
 </script>
 
 <style scoped lang="scss">
+    @keyframes spin {
+        0% {transform: rotate(0deg)}
+        100% {transform: rotate(360deg)}
+    }
     .v-button{
         font-size: var(--font-szie);height: var(--button-height);padding: 0 1em;border-radius: var(--border-radius);border: 1px solid var(--border-color);
         background: var(--button-bg);display: inline-flex;justify-content: center;align-items: center;vertical-align: middle;
@@ -43,6 +43,9 @@
         &.icon-right{
             >.icon{order: 2;margin-right: 0;margin-left: .1em}
             >.content{order: 1}
+        }
+        .loading{
+            animation: spin 1s infinite linear;
         }
     }
 </style>
