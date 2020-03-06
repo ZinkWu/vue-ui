efccb8d8f2c7ac6748b2466d6b93d93e76156e6a
ZinkWu <wugu2599l@gmail.com>
2020-03-06 22:12:41 +0800

    完成 input 基本样式

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from "vue"
 import Button from "./Button";
 import Icon from "./Icon";
 import ButtonGroup from "./ButtonGroup";
+import Input from "./Input";
 
 Vue.component("v-button", Button)
 Vue.component("v-button-group", ButtonGroup)
 Vue.component("v-icon", Icon)
+Vue.component('v-input', Input)
 
 new Vue({
   el: "#app",
